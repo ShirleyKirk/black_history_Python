28409738f323851fb2f55d05156a371c54a5a559
Oreki Yuki <lianleiiswho@yeah.net>
2016-03-28 11:19:53 +0800

    mutex version-进程锁

--- Python_system/Processes/thread-count.py
+++ Python_system/Processes/thread-count.py
@@ -3,10 +3,15 @@ import _thread as thread,time
 def counter(myID,count):
 	for i in range(count):
 		time.sleep(1)
+		mutex.acquire()
 		print('[%s] => %s '%(myID,i))
+		mutex.release()
+
+mutex=thread.allocate_lock()
 
 for x in range(5):
 	thread.start_new_thread(counter,(1,5))
-	time.sleep(6)
-	print('Main thread exiting.\n')
+
+time.sleep(6)
+print('Main thread exiting.\n')
 
